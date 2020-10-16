771d74cebc1fc439b6142c2ab9e9afeda8efea20
Abhay Gangwar <abhaygangwar3@gmail.com>
2020-10-16 21:27:31 +0530

    Actual program not rubbish

--- YoYo.R
+++ YoYo.R
@@ -1,3 +1,10 @@
-print("Raat akeli hai!!!")
-print("Bujh gye diye")
-print("Hum tum yahan")
+columnMeans <- function(vec, removeNA = TRUE)
+{
+  col = ncol(vec)
+  means = vector(mode = "numeric", length = col)
+  for(i in 1:col)
+  {
+    means[i] <- mean(vec[, i], na.rm = removeNA)
+  }
+  means
+}
